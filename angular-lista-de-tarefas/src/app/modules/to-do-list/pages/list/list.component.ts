93c635db25c763e2bddbf4f0e5dd86e0ed69bd99
BiancalBarreto <bianca.lb2004@gmail.com>
2025-03-12 15:08:33 -0300

    new component - input add item

--- angular-lista-de-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
+++ angular-lista-de-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+//components
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  public addItem = signal(true); 
 
 }
